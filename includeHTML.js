65dcfa07703be41cfb70f865aee1e67fe6dc677e
Red <redonx99@gmail.com>
2026-02-23 18:35:12 -0500

    Execute scripts in included HTML so footer modal forms bind correctly

--- includeHTML.js
+++ includeHTML.js
@@ -3,13 +3,31 @@
 ; Title: includeHTML.js
 ; Author: W3 Schools
 ; Date Created: 28 January 2023
-; Last Updated: 04 February 2023
+; Last Updated: 23 February 2026
 ; Modified By: Red
 ; Description: This code houses JavaScript for the light & dark themes
 ; Sources Used: W3 Schools How TO - Include HTML, URL: https://www.w3schools.com/howto/howto_html_include.asp
 ;=====================================
 */
 
+
+function runIncludedScripts(container) {
+  var scripts = container.getElementsByTagName("script");
+  var i;
+  for (i = 0; i < scripts.length; i++) {
+    var oldScript = scripts[i];
+    var newScript = document.createElement("script");
+    var j;
+
+    for (j = 0; j < oldScript.attributes.length; j++) {
+      var attr = oldScript.attributes[j];
+      newScript.setAttribute(attr.name, attr.value);
+    }
+
+    newScript.text = oldScript.text || oldScript.textContent || "";
+    oldScript.parentNode.replaceChild(newScript, oldScript);
+  }
+}
 
 function includeHTML() {
   var z, i, elmnt, file, xhttp;
@@ -24,13 +42,16 @@ function includeHTML() {
       xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+            runIncludedScripts(elmnt);
+          }
           if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
           /*remove the attribute, and call this function once more:*/
           elmnt.removeAttribute("w3-include-html");
           includeHTML();
         }
-      }      
+      }
       xhttp.open("GET", file, true);
       xhttp.send();
       /*exit the function:*/
